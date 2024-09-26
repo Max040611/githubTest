1d3f9122e4db26bfa4c7fe0e93c6619a288b405e
max <maldosari24@twq.idserve.net>
2024-09-26 16:45:27 +0300

    Initial Commit

---
+++ githubTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  githubTest
+//
+//  Created by Mashael Aldosari on 23/03/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ githubTest/githubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubTestApp.swift
+//  githubTest
+//
+//  Created by Mashael Aldosari on 23/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct githubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
